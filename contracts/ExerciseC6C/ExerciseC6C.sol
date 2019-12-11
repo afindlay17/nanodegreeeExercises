26503e1183a938096c2aa523bb7ce4840b58f42a
afindl3 <alisonfindlay93@hotmail.com>
2019-12-11 14:12:22 -0500

    ExerciseC6C - completed, tests not running

--- contracts/ExerciseC6C/ExerciseC6C.sol
+++ contracts/ExerciseC6C/ExerciseC6C.sol
@@ -6,168 +6,86 @@ pragma solidity ^0.4.25;
 
 import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-
 contract ExerciseC6C {
-    using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
+  using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
 
-    /********************************************************************************************/
-    /*                                       DATA VARIABLES                                     */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                       DATA VARIABLES                                     */
+  /********************************************************************************************/
 
-    struct Profile {
-        string id;
-        bool isRegistered;
-        bool isAdmin;
-        uint256 sales;
-        uint256 bonus;
-        address wallet;
-    }
+  struct Profile {
+      string id;
+      bool isRegistered;
+      bool isAdmin;
+      uint256 sales;
+      uint256 bonus;
+      address wallet;
+  }
 
-    address private contractOwner;              // Account used to deploy contract
-    mapping(string => Profile) employees;      // Mapping for storing employees
+  address private contractOwner;              // Account used to deploy contract
+  mapping(string => Profile) employees;      // Mapping for storing employees
 
-    /********************************************************************************************/
-    /*                                       EVENT DEFINITIONS                                  */
-    /********************************************************************************************/
+  /**
+  * @dev Constructor
+  * The deploying account becomes contractOwner
+  */
+  constructor() public {
+    contractOwner = msg.sender;
+  }
 
-    // No events
+  /********************************************************************************************/
+  /*                                       FUNCTION MODIFIERS                                 */
+  /********************************************************************************************/
 
-    /**
-    * @dev Constructor
-    *      The deploying account becomes contractOwner
-    */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
-        contractOwner = msg.sender;
-    }
+  // Modifiers help avoid duplication of code. They are typically used to validate something
+  // before a function is allowed to be executed.
 
-    /********************************************************************************************/
-    /*                                       FUNCTION MODIFIERS                                 */
-    /********************************************************************************************/
+  /**
+  * @dev Modifier that requires the "ContractOwner" account to be the function caller
+  */
+  modifier requireContractOwner() {
+    require(msg.sender == contractOwner, "Caller is not contract owner");
+    _;
+  }
 
-    // Modifiers help avoid duplication of code. They are typically used to validate something
-    // before a function is allowed to be executed.
+  /********************************************************************************************/
+  /*                                       UTILITY FUNCTIONS                                  */
+  /********************************************************************************************/
 
-    /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner()
-    {
-        require(msg.sender == contractOwner, "Caller is not contract owner");
-        _;
-    }
+  /**
+  * @dev Check if an employee is registered
+  * @return A bool that indicates if the employee is registered
+  */   
+  function isEmployeeRegistered(string id) external view returns(bool) {
+    return employees[id].isRegistered;
+  }
 
-    /********************************************************************************************/
-    /*                                       UTILITY FUNCTIONS                                  */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                     SMART CONTRACT FUNCTIONS                             */
+  /********************************************************************************************/
 
-   /**
-    * @dev Check if an employee is registered
-    *
-    * @return A bool that indicates if the employee is registered
-    */   
-    function isEmployeeRegistered
-                            (
-                                string id
-                            )
-                            external
-                            view
-                            returns(bool)
-    {
-        return employees[id].isRegistered;
-    }
+  function registerEmployee(string id, bool isAdmin, address wallet) external requireContractOwner {
+    require(!employees[id].isRegistered, "Employee is already registered.");
 
-    /********************************************************************************************/
-    /*                                     SMART CONTRACT FUNCTIONS                             */
-    /********************************************************************************************/
+    employees[id] = Profile({
+      id: id,
+      isRegistered: true,
+      isAdmin: isAdmin,
+      sales: 0,
+      bonus: 0,
+      wallet: wallet
+    });
+  }
 
-    function registerEmployee
-                                (
-                                    string id,
-                                    bool isAdmin,
-                                    address wallet
-                                )
-                                external
-                                requireContractOwner
-    {
-        require(!employees[id].isRegistered, "Employee is already registered.");
+  function getEmployeeBonus(string id) external view requireContractOwner returns(uint256) {
+    return employees[id].bonus;
+  }
 
-        employees[id] = Profile({
-                                        id: id,
-                                        isRegistered: true,
-                                        isAdmin: isAdmin,
-                                        sales: 0,
-                                        bonus: 0,
-                                        wallet: wallet
-                                });
-    }
+  function updateEmployee(string id, uint256 sales, uint256 bonus) external {
+    require(employees[id].isRegistered, "Employee is not registered.");
 
-    function getEmployeeBonus
-                            (
-                                string id
-                            )
-                            external
-                            view
-                            requireContractOwner
-                            returns(uint256)
-    {
-        return employees[id].bonus;
-    }
-
-    function updateEmployee
-                                (
-                                    string id,
-                                    uint256 sales,
-                                    uint256 bonus
-
-                                )
-                                internal
-                                requireContractOwner
-    {
-        require(employees[id].isRegistered, "Employee is not registered.");
-
-        employees[id].sales = employees[id].sales.add(sales);
-        employees[id].bonus = employees[id].bonus.add(bonus);
-
-    }
-
-    function calculateBonus
-                            (
-                                uint256 sales
-                            )
-                            internal
-                            view
-                            requireContractOwner
-                            returns(uint256)
-    {
-        if (sales < 100) {
-            return sales.mul(5).div(100);
-        }
-        else if (sales < 500) {
-            return sales.mul(7).div(100);
-        }
-        else {
-            return sales.mul(10).div(100);
-        }
-    }
-
-    function addSale
-                                (
-                                    string id,
-                                    uint256 amount
-                                )
-                                external
-                                requireContractOwner
-    {
-        updateEmployee(
-                        id,
-                        amount,
-                        calculateBonus(amount)
-        );
-    }
-
+    employees[id].sales = employees[id].sales.add(sales);
+    employees[id].bonus = employees[id].bonus.add(bonus);
+  }
 
 }
--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "clever twice seed rural liquid unique reopen laundry best earn know segment";
 
 module.exports = {
   networks: {
