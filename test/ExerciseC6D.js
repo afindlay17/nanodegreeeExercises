8fa1cdfeb48959eafa44d511dd1708ac0e9429de
afindl3 <alisonfindlay93@hotmail.com>
2019-12-19 17:34:06 -0500

    Exercise C6D complete - tests passing

--- test/ExerciseC6D.js
+++ test/ExerciseC6D.js
@@ -2,8 +2,7 @@
 var Test = require('../config/testConfig.js');
 //var BigNumber = require('bignumber.js');
 
-contract('ExerciseC6D', async (accounts) => {
-
+contract('ExerciseC6D', async (accounts) => { // accounts from truffle.js
   const TEST_ORACLES_COUNT = 20;
   var config;
   before('setup contract', async () => {
@@ -27,18 +26,18 @@ contract('ExerciseC6D', async (accounts) => {
 
 
   it('can register oracles', async () => {
-    
     // ARRANGE
     let fee = await config.exerciseC6D.REGISTRATION_FEE.call();
 
     // ACT
     for(let a=1; a<TEST_ORACLES_COUNT; a++) {      
       await config.exerciseC6D.registerOracle({ from: accounts[a], value: fee });
+      let result = await config.exerciseC6D.getOracle.call(accounts[a]);
+      console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}` )
     }
   });
 
   it('can request flight status', async () => {
-    
     // ARRANGE
     let flight = 'ND1309'; // Course number
     let timestamp = Math.floor(Date.now() / 1000);
--- truffle.js
+++ truffle.js
@@ -1,14 +1,17 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "scheme inherit misery rain truly borrow taste cereal give capable toward bullet";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50); 
+        // 50 - number of accounts, must be greater than TEST_ORACLE_COUNT
+        // Repreents the 'accounts' param in ExerciseC6D.js
+        // run ganache-cli -a 50 if runnning into the error 'Error: sender doesn't have enough funds to send tx. The upfront cost is: 1999999900000000000 and the sender's account only has: 0'
       },
       network_id: '*',
-      gas: 9999999
+      gas: 9999999 // run ganache-cli -l 9999999 if running into error 'Exceeds block gas limit'
     }
   },
   compilers: {
